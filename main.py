9f25035786bf6cf0a86051e0e90ded60f446556b
Jhonata Medley <jhonatamedley@gmail.com>
2026-04-01 01:00:45 -0300

    introdução do ST no codigo

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import requests
 import yfinance as yf
+import streamlit as st
+
+#titulo da pagina
+st.title("A gigantes empresas")
+
 
 #siglas das empresas
-empresas = ['NVDA', 'AAPL', 'GOOG', 'MSFT', 'AMZN']
+empresas = ['NVDA', 'TSLA']
 
 
 data = []
 
 for empresa in empresas:
+
+    caixa = st.container()
+
+
     stock = yf.Ticker(empresa)
     informacao = stock.info
     nome = informacao['displayName']
     seguimento = informacao['industry']
     valorCota = informacao['currentPrice']
     valorMercado = informacao['marketCap']
+    
 
-    print(f"A empresa {nome} que atua no seguimento de {seguimento} fechou o dia com a cota a {valorCota} e seu valor de mercado é de {valorMercado}")
-    print("-" * 40)
 
+    caixa.write(f"A empresa {nome} que atua no seguimento de {seguimento} fechou o dia com a cota a {valorCota:.2f} e seu valor de mercado é de {valorMercado:.2f}")
+    caixa.write("--------------------------------------------------")
+    caixa.space("large")
 
 
 
